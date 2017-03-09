e2bad4bda00df7216dc843c111db5fadcafbe25a
Jonathanz87 <jonathanz@foxmail.com>
2017-03-08 20:11:50 -0800

    problem 14

---
+++ java/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+/*
+	problem 14
+	Write a function to find the longest common prefix string amongst an array of strings.
+*/
+
+public class LongestCommonPrefix{
+	static public void main(String[] args){
+
+	}
+
+	static public String longestCommonPrefix(String[] strs) {
+		
+	}
+}
