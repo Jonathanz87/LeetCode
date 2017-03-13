e7383e342ed73215d7ae8689656f18e3e4f686ba
Jonathanz87 <jonathanz@foxmail.com>
2017-03-12 19:48:01 -0700

    problem 22

--- java/GenerateParentheses.java
+++ java/GenerateParentheses.java
@@ -23,11 +23,92 @@ public class GenerateParentheses{
 			long startTime=System.nanoTime();
 			generateParenthesis(Integer.parseInt(args[0]));
 			System.out.println("time: "+(System.nanoTime()-startTime)+"ns");
-		}else{
+		}else if(args[1].equals("2")){
 			long startTime=System.nanoTime();
 			generateParenthesis2(Integer.parseInt(args[0]));
 			System.out.println("time: "+(System.nanoTime()-startTime)+"ns");
+		}else if(args[1].equals("3")){
+			long startTime=System.nanoTime();
+			generateParenthesis3(Integer.parseInt(args[0]));
+			System.out.println("time: "+(System.nanoTime()-startTime)+"ns");
 		}
+		else if(args[1].equals("4")){
+			long startTime=System.nanoTime();
+			generateParenthesis4(Integer.parseInt(args[0]));
+			System.out.println("time: "+(System.nanoTime()-startTime)+"ns");
+		}
+	}
+
+	static private class StringQuequ{
+		private String val;
+		private int left, right;
+		private StringQuequ next;
+		private StringQuequ(String val, int left, int right){
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	static public List<String> generateParenthesis4(int n) {
+		List<String> result = new ArrayList<String>();
+		int size = n * n * n;
+		String[] str = new String[size];
+		int[] left = new int[size];
+		int[] right = new int[size];
+
+		int head = 0, tail = 0;
+		int len = n * 2;
+		str[head] = "(";
+		left[head] = n - 1;
+		right[head] = n;
+
+		while(head <= tail){
+			if(str[head].length() >= len){
+				result.add(str[head]);
+				head++;
+				continue;
+			}
+
+			if(left[head] > 0){
+				str[++tail] = str[head] + "(";
+				left[tail] = left[head] - 1;
+				right[tail] = right[head];
+			}
+			if(right[head] > left[head]){
+				str[++tail] = str[head] + ")";
+				left[tail] = left[head];
+				right[tail] = right[head] - 1;
+			}
+			head++;
+		}
+		return result;
+	}
+
+	static public List<String> generateParenthesis3(int n) {
+		List<String> result = new ArrayList<String>();
+		StringQuequ head = new StringQuequ("(", n - 1, n);
+		StringQuequ tail = head;
+		int len = n * 2;
+
+		while(head != null){
+			if(head.val.length() >= len){
+				result.add(head.val);
+				head = head.next;
+				continue;
+			}
+
+			if(head.left > 0){
+				tail.next = new StringQuequ(head.val + "(", head.left - 1, head.right);
+				tail = tail.next;
+			}
+			if(head.right > head.left){
+				tail.next = new StringQuequ(head.val + ")", head.left, head.right - 1);
+				tail = tail.next;
+			}
+			head = head.next;
+		}
+		return result;
 	}
 
 	static public List<String> generateParenthesis(int n) {
@@ -65,7 +146,7 @@ public class GenerateParentheses{
 		return result;
 	}
 
-	static public ArrayList<String> generateParenthesis2(int n) {
+	static public List<String> generateParenthesis2(int n) {
         ArrayList<String> result = new ArrayList<String>();
         if (n <= 0) {
             return result;
