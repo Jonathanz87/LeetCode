24969826c0fdbbc978aa5b0482f4eb984d8815d1
Jonathanz87 <jonathanz@foxmail.com>
2017-03-13 20:48:59 -0700

    problem 26

--- java/RemoveDuplicatesfromSortedArray.java
+++ java/RemoveDuplicatesfromSortedArray.java
@@ -29,6 +29,7 @@ public class RemoveDuplicatesfromSortedArray{
 		System.out.println();
 	}
 	static public int removeDuplicates(int[] nums) {
+		if(nums.length <= 0) return 0;
 		int index = 0;
 
 		for(int i = 1; i < nums.length; i++){
