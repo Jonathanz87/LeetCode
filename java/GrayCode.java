ec517bce7b5a34c08970085ac8efa53d4912556f
jonathanz87 <jonathanz@foxmail.com>
2017-04-24 17:26:01 -0700

    problem 89

---
+++ java/GrayCode.java
@@ -0,0 +1,49 @@
+/*
+	problem 89
+	The gray code is a binary numeral system 
+	where two successive values differ in only one bit.
+	Given a non-negative integer n representing the total number of bits in the code, 
+	print the sequence of gray code. A gray code sequence must begin with 0.
+	For example, given n = 2, return [0,1,3,2]. 
+	Its gray code sequence is:
+		00 - 0
+		01 - 1
+		11 - 3
+		10 - 2
+	Note:
+	For a given n, a gray code sequence is not uniquely defined.
+	For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+	For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+*/
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GrayCode{
+	public static void main(String[] args){
+		System.out.println(grayCode(Integer.parseInt(args[0])));
+	}
+
+	public static List<Integer> grayCode(int n){
+		List<Integer> codes = new ArrayList<Integer>();
+		grayCodeHelper(codes, 0, 0, n);
+		return codes;
+	}
+
+	public static void grayCodeHelper(List<Integer> codes, int num, int d, int n){
+		if(d < n){
+			int temp = num;
+			int bit = 0;
+			while(temp > 0){
+				bit ^= temp;
+				temp = temp >> 1;
+			}
+			bit = bit & 0x1;
+
+			grayCodeHelper(codes, ((num << 1) | bit), d + 1, n);
+			grayCodeHelper(codes, ((num << 1) | (~bit & 0x1)), d + 1, n);
+		}else{
+			codes.add(num);
+		}
+	}
+}
