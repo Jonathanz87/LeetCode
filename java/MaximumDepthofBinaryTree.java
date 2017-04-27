b554e931282f186741f83b51019ea9c0849732bc
jonathanz87 <jonathanz@foxmail.com>
2017-04-26 18:39:18 -0700

    problem 104

---
+++ java/MaximumDepthofBinaryTree.java
@@ -0,0 +1,21 @@
+/*
+	problem 104
+	Given a binary tree, find its maximum depth.
+	The maximum depth is the number of nodes along the longest path 
+	from the root node down to the farthest leaf node.
+*/
+
+public class MaximumDepthofBinaryTree{
+
+	//Definition for a binary tree node.
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+
+	public static int maxDepth(TreeNode root){
+		
+	}
+}
