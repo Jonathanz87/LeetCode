5ec691ed1a6c84414ae39cdfa8b924493af6230f
= <=>
2017-06-16 16:23:29 -0400

    problem 51

--- java/NQueens.java
+++ java/NQueens.java
@@ -1,5 +1,5 @@
 /*
-	problem 53
+	problem 51
 	The n-queens puzzle is the problem of placing n queens on an n×n chessboard 
 	such that no two queens attack each other.
 	Given an integer n, return all distinct solutions to the n-queens puzzle.
