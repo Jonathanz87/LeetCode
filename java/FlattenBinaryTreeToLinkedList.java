56e73796308ff0cead2cd31eee1d05611789a4a7
cezhang <jonathanz@foxmail.com>
2017-09-16 15:24:00 -0400

    problem 114

--- java/FlattenBinaryTreeToLinkedList.java
+++ java/FlattenBinaryTreeToLinkedList.java
@@ -1,6 +1,6 @@
 /*
 	problem 114
-	Given a binary tree, 
+	Given a binary tree,
 	flatten it to a linked list in-place.
 	For example,
 	Given
@@ -23,7 +23,7 @@
 	          6
 */
 
-public class FlattenBinaryTreeToLinkedList{
+public class FlattenBinaryTreeToLinkedList {
 
 	//Definition for a binary tree node.
 	public static class TreeNode {
@@ -33,7 +33,38 @@ public class FlattenBinaryTreeToLinkedList{
 		TreeNode(int x) { val = x;}
 	}
 
-	public void flatten(TreeNode root) {
+	public static TreeNode tail = null;
 
+	public static void flatten(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		if (root.left != null) {
+			TreeNode temp = root.right;
+			root.right = root.left;
+			root.left = temp;
+		}
+		flatten(root.right);
+
+		if(root.left != null){
+			tail.right = root.left;
+			root.left = null;
+			flatten(tail.right);
+		}
+
+		if(root.right == null){
+			tail = root;
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
