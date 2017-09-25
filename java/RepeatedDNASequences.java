b8d991f198a3db0f07197f8c8c797ed7919b8404
cezhang <jonathanz@foxmail.com>
2017-09-24 20:05:15 -0400

    problem 187 bitMap

--- java/RepeatedDNASequences.java
+++ java/RepeatedDNASequences.java
@@ -18,13 +18,43 @@ import java.util.TreeSet;
 import java.util.HashSet;
 public class RepeatedDNASequences {
 	public static void main(String[] args) {
-		for (String s : findRepeatedDnaSequencesHashMap(args[0])) {
+		//System.out.println(getIndex(args[0]));
+		for (String s : findRepeatedDnaSequencesBitMap(args[0])) {
 			System.out.println(s);
 		}
 	}
 
 	public static List<String> findRepeatedDnaSequencesBitMap(String s) {
-		return null;
+		//32768 = 4 ^ 10 / 32
+		int[] seqBitMap = new int[32768];
+		Set<String> resultSet = new TreeSet<>();
+
+		for (int i = 0, len = s.length() - 9; i < len; i++) {
+			String seq = s.substring(i, i + 10);
+			int index = getIndex(seq);
+			int mapIndex = index / 32;
+			int bitIndex = index % 32;
+			if (((seqBitMap[mapIndex] >> bitIndex) & 1) == 1) {
+				resultSet.add(seq);
+			}
+			seqBitMap[mapIndex] |= (1 << bitIndex);
+		}
+
+		return new ArrayList<String>(resultSet);
+	}
+
+	private static int getIndex(String s) {
+		int index = 0;
+		for (int i = 0, len = s.length(); i < len; i++) {
+			index <<= 2;
+			switch (s.charAt(i)) {
+			// case 'A' : index &= 0; break;
+			case 'C' : index |= 1; break;
+			case 'G' : index |= 2; break;
+			case 'T' : index |= 3; break;
+			}
+		}
+		return index;
 	}
 
 	public static List<String> findRepeatedDnaSequencesHashMap(String s) {
@@ -34,13 +64,12 @@ public class RepeatedDNASequences {
 			String seq = s.substring(i, i + 10);
 			if (seqSet.contains(seq)) {
 				resultSet.add(seq);
-			} else{
+			} else {
 				seqSet.add(seq);
 			}
 		}
 		return new ArrayList<String>(resultSet);
 	}
-
 
 	public static List<String> findRepeatedDnaSequences(String s) {
 		//0 = A; 1 = C; 2 = G; 3 = T
