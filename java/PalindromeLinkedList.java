20b452ad6bc8c25cbfb12948810e0308f751a648
Jonathanz87 <jonathanz@foxmail.com>
2017-09-28 17:42:11 -0400

    problem 234

---
+++ java/PalindromeLinkedList.java
@@ -0,0 +1,24 @@
+/*
+	problem 234
+	Given a singly linked list, determine if it is a palindrome.
+	Follow up:
+	Could you do it in O(n) time and O(1) space?
+*/
+
+public class PalindromeLinkedList{
+	// Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+
+	public boolean isPalindrome(ListNode head) {
+		int size = 0;
+		ListNode currentNode = head;
+		ListNode tail = null;
+		while(currentNode != null){
+			
+		}
+	}
+}
