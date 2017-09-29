03bc7f63eacec2f82229c110b165b20cfcce5bbf
cezhang <jonathanz@foxmail.com>
2017-09-28 21:59:16 -0400

    problem 226

--- java/InvertBinaryTree.java
+++ java/InvertBinaryTree.java
@@ -29,7 +29,9 @@ public class InvertBinaryTree {
 
 	public TreeNode invertTree(TreeNode root) {
 		if (root == null) return root;
-
-		root.left
+		TreeNode temp = invertTree(root.left);
+		root.left = invertTree(root.right);
+		root.right = temp;
+		return root;
 	}
 }
