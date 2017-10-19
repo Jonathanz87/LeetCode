fbb6b87655a293f612ca100e545eb34dc842aa57
Jonathanz87 <jonathanz@foxmail.com>
2017-10-19 16:45:45 -0400

    problem 207

---
+++ java/CourseSchedule.java
@@ -0,0 +1,60 @@
+/*
+	problem 207
+	There are a total of n courses you have to take,
+	labeled from 0 to n - 1.
+	Some courses may have prerequisites,
+	for example to take course 0 you have to first take course 1,
+	which is expressed as a pair: [0,1]
+	Given the total number of courses and a list of prerequisite pairs,
+	is it possible for you to finish all courses?
+	For example:
+
+		2, [[1,0]]
+		There are a total of 2 courses to take.
+		To take course 1 you should have finished course 0.
+		So it is possible.
+
+		2, [[1,0],[0,1]]
+		There are a total of 2 courses to take.
+		To take course 1 you should have finished course 0,
+		and to take course 0 you should also have finished course 1.
+		So it is impossible.
+	Note:
+	The input prerequisites is a graph represented by a list of edges,
+	not adjacency matrices.
+	Read more about how a graph is represented.
+	You may assume that there are no duplicate edges in the input prerequisites.
+*/
+import java.util.List;
+import java.util.LinkedList;
+
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List[] prerequisiteArray = new List[numCourses];
+        int[] degreeArray = int[numCourses];
+        int[] zeroDegreeStack = new int[numCourses];
+        int[] visitedStack = new int[numCourses];
+        int zeroDegreeStackIndex = -1, visitedStackIndex = -1;
+
+        for(int[] prerequest : prerequisites) {
+            if(prerequisiteArray[prerequest[1]] == null) {
+                prerequisiteArray[prerequest[1]] = new LinkedList<>();
+            }
+            prerequisiteArray[prerequest[1]].add(prerequest[0]);
+            degreeArray[prerequest[1]]++;
+        }
+
+        for(int i = 0; i < degreeArray.length; i++) {
+            if(degreeArray[i] == 0) {
+                zeroDegreeStack[++zeroDegreeStackIndex] = i;
+            }
+        }
+        while(zeroDegreeStackIndex > -1) {
+        	int index = zeroDegreeStack[zeroDegreeStackIndex--];
+        	visitedStack[++visitedStackIndex] = index;
+        	
+        }
+
+        return false;
+    }
+}
