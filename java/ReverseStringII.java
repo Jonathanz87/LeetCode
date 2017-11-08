b8e9e83a285a3a269e307a3a0bbc6a6c582df0be
Jonathanz87 <jonathanz@foxmail.com>
2017-11-08 17:16:28 -0500

    proble 541

---
+++ java/ReverseStringII.java
@@ -0,0 +1,38 @@
+/*
+	problem 541
+	Given a string and an integer k,
+	you need to reverse the first k characters for every 2k characters
+	counting from the start of the string.
+	If there are less than k characters left,
+	reverse all of them. If there are less than 2k
+	but greater than or equal to k characters,
+	then reverse the first k characters and left the other as original.
+	Example:
+		Input: s = "abcdefg", k = 2
+		Output: "bacdfeg"
+	Restrictions:
+		The string consists of lower English letters only.
+		Length of the given string and k will in the range [1, 10000]
+*/
+
+public class ReverseStringII {
+    public String reverseStr(String s, int k) {
+        if(s == null || k <= 1 ) {
+            return s;
+        }
+        char[] letterArray = s.toCharArray();
+
+        for(int i = 0; i < letterArray.length; i += k * 2) {
+            int leftIndex = i, rightIndex = Math.min(i + k, letterArray.length) - 1;
+            while(leftIndex < rightIndex) {
+                letterArray[leftIndex] = (char)(letterArray[leftIndex] ^ letterArray[rightIndex]);
+                letterArray[rightIndex] = (char)(letterArray[leftIndex] ^ letterArray[rightIndex]);
+                letterArray[leftIndex] = (char)(letterArray[leftIndex] ^ letterArray[rightIndex]);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+
+        return String.valueOf(letterArray);
+    }
+}
