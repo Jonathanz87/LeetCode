67716c8a432110f9408d201cbcfe5bf14a34196e
cezhang <jonathanz@foxmail.com>
2017-11-18 12:01:20 -0500

    problem 42

--- java/TrappingRainWater.java
+++ java/TrappingRainWater.java
@@ -8,38 +8,81 @@
 */
 
 public class TrappingRainWater {
-    public int trap(int[] height) {
-        int totalArea = 0;
-        int leftIndex = 0, rightIndex = height.length - 1;
-        while (leftIndex < rightIndex && height[leftIndex] <= height[leftIndex + 1]) {
-            leftIndex++;
-        }
+	public int trap(int[] height) {
+		int totalArea = 0;
+		int leftIndex = 0, rightIndex = height.length - 1;
+		while (leftIndex < rightIndex && height[leftIndex] <= height[leftIndex + 1]) {
+			leftIndex++;
+		}
 
-        while(rightIndex > leftIndex && height[rightIndex - 1] >= height[rightIndex]) {
-            rightIndex--;
-        }
+		while (rightIndex > leftIndex && height[rightIndex - 1] >= height[rightIndex]) {
+			rightIndex--;
+		}
 
-        while(leftIndex < rightIndex) {
-            int area = 0;
-            if(height[leftIndex] < height[rightIndex]) {
-                int i = leftIndex + 1;
-                while(i < rightIndex && height[leftIndex] >= height[i]) {
-                    area += height[leftIndex] - height[i];
-                    i++;
-                }
+		while (leftIndex < rightIndex) {
+			int area = 0;
+			if (height[leftIndex] < height[rightIndex]) {
+				int i = leftIndex + 1;
+				while (i < rightIndex && height[leftIndex] >= height[i]) {
+					area += height[leftIndex] - height[i];
+					i++;
+				}
 
-                leftIndex = i;
-            } else {
-                int i = rightIndex - 1;
-                while(i > leftIndex && height[i] <= height[rightIndex]) {
-                    area += height[rightIndex] - height[i];
-                    i--;
-                }
+				leftIndex = i;
+			} else {
+				int i = rightIndex - 1;
+				while (i > leftIndex && height[i] <= height[rightIndex]) {
+					area += height[rightIndex] - height[i];
+					i--;
+				}
 
-                rightIndex = i;
-            }
-            totalArea += area;
-        }
-        return totalArea;
-    }
+				rightIndex = i;
+			}
+			totalArea += area;
+		}
+		return totalArea;
+	}
+
+	/*
+		solution
+		set the most left as maxLeft and most right as maxRight
+		if 		maxLeft < maxRight
+		then	any bar that smaller than maxLeft traps water
+				or any bar more higher than maxLeft does not trap water, 
+				in this case, set it to new MaxLeft
+		else	the same
+						|
+		|	w			|
+		|	w			|
+		|	|			|
+		|	|			|
+		|	|			|
+	*/
+
+	public static int trap2(int[] height) {
+		if (height == null || height.length <= 0) return 0;
+		int leftIndex = 0, rightIndex = height.length - 1;
+		int maxLeft = height[leftIndex++], maxRight = height[rightIndex--];
+		int water = 0;
+		while (leftIndex <= rightIndex) {
+			if (maxLeft < maxRight) {
+				if (height[leftIndex] > maxLeft) {
+					maxLeft = height[leftIndex];
+				} else {
+					water += maxLeft - height[leftIndex];
+				}
+				leftIndex++;
+			} else {
+				if (height[rightIndex] > maxRight) {
+					maxRight = height[rightIndex];
+				} else {
+					water += maxRight - height[rightIndex];
+				}
+				rightIndex--;
+			}
+		}
+
+		return water;
+	}
+
 }
