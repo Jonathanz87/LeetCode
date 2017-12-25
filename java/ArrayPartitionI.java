e8162592f0833767535f104365e050087812b9ee
jonathanz87 <jonathanz@foxmail.com>
2017-12-24 21:01:04 -0500

    problem 561

---
+++ java/ArrayPartitionI.java
@@ -0,0 +1,30 @@
+/*
+	problem 561
+	Given an array of 2n integers,
+	your task is to group these integers into n pairs of integer,
+	say (a1, b1), (a2, b2), ..., (an, bn)
+	which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+	Example 1:
+		Input: [1,4,3,2]
+		Output: 4
+		Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+	Note:
+		n is a positive integer, which is in the range of [1, 10000].
+		All the integers in the array will be in the range of [-10000, 10000].
+*/
+
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+	public int arrayPairSum(int[] nums) {
+		if (nums == null) return 0;
+		int sum = 0;
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length; i += 2) {
+			sum += nums[i];
+		}
+
+		return sum;
+	}
+}
