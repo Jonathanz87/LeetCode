e73a4875c3378338e6e67978a6fc2719ffcaebfb
jonathanz87 <jonathanz@foxmail.com>
2018-01-14 14:03:20 -0500

    problem 232

--- java/ImplementQueueUsingStacks.java
+++ java/ImplementQueueUsingStacks.java
@@ -26,30 +26,47 @@ public class ImplementQueueUsingStacks {
 		* boolean param_4 = obj.empty();
 	*/
 	private static class MyQueue {
+		private Stack<Integer> stack1;
+		private Stack<Integer> stack2;
 
 		/** Initialize your data structure here. */
 		public MyQueue() {
-
+			stack1 = new Stack<>();
+			stack2 = new Stack<>();
 		}
 
 		/** Push element x to the back of queue. */
 		public void push(int x) {
-
+			stack1.push(x);
 		}
 
 		/** Removes the element from in front of queue and returns that element. */
 		public int pop() {
-
+			if(stack2.isEmpty()){
+				transfer();
+			}
+			return stack2.pop();
 		}
 
 		/** Get the front element. */
 		public int peek() {
-
+			if(stack2.isEmpty()){
+				transfer();
+			}
+			return stack2.peek();
 		}
 
 		/** Returns whether the queue is empty. */
 		public boolean empty() {
+			return stack2.isEmpty() && stack1.isEmpty();
+		}
 
+		private void transfer(){
+			if(stack2.isEmpty()){
+				while(!stack1.isEmpty()){
+					stack2.push(stack1.pop());
+				}
+			}
 		}
 	}
 }
