5fb9d047407cef377e6e607ddd3a49ece7a3cc2a
jonathanz87 <jonathanz@foxmail.com>
2018-01-16 20:38:07 -0500

    problem 402

---
+++ java/RemoveKDigits.java
@@ -0,0 +1,55 @@
+/*
+	problem 402
+	Given a non-negative integer num represented as a string, 
+	remove k digits from the number so that the new number is the smallest possible.
+	Note:
+		The length of num is less than 10002 and will be ≥ k.
+		The given num does not contain any leading zero.
+	Example 1:
+		Input: num = "1432219", k = 3
+		Output: "1219"
+		Explanation: Remove the three digits 4, 3, and 2 to 
+		form the new number 1219 which is the smallest.
+	Example 2:
+		Input: num = "10200", k = 1
+		Output: "200"
+		Explanation: Remove the leading 1 and the number is 200. 
+		Note that the output must not contain leading zeroes.
+	Example 3:
+		Input: num = "10", k = 2
+		Output: "0"
+		Explanation: Remove all the digits from the number 
+		and it is left with nothing which is 0.
+*/
+
+public class RemoveKDigits {
+	public static String removeKdigits(String num, int k) {
+		if (num.length() <= k) return "0";
+
+		final int NUM_LEN = num.length();
+		final int STACK_LEN = NUM_LEN - k;
+		int numIndex = 1;
+		int stackIndex = -1;
+		char[] numChar = num.toCharArray();
+		char[] digitsStack = new char[STACK_LEN];
+
+		digitsStack[++stackIndex] = numChar[0];
+
+		while (k > 0 && numIndex < NUM_LEN) {
+			while (k > 0 && stackIndex >= 0 && digitsStack[stackIndex] > numChar[numIndex]) {
+				stackIndex--;
+				k--;
+			}
+			if (stackIndex < STACK_LEN - 1) {
+				digitsStack[++stackIndex] = numChar[numIndex];
+			}
+			numIndex++;
+		}
+
+		while (numIndex < NUM_LEN) {
+			digitsStack[++stackIndex] = numChar[numIndex++];
+		}
+
+		return new String(digitsStack);
+	}
+}
