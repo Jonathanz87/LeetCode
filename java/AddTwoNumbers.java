c3615fcebdf6b8e8b13cf5d46c16c31ed89389c1
Jonathanz87 <Jonathanz87@users.noreply.github.com>
2018-03-01 22:40:08 -0800

    problem 2

--- java/AddTwoNumbers.java
+++ java/AddTwoNumbers.java
@@ -3,15 +3,13 @@
 	You are given two non-empty linked lists representing two non-negative integers.
 	The digits are stored in reverse order and each of their nodes contain a single digit.
 	Add the two numbers and return it as a linked list.
-
 	You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
 	Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 	Output: 7 -> 0 -> 8
 */
 
-public class AddTwoNumbers{
-	public static void main(String[] args){
+public class AddTwoNumbers {
+	public static void main(String[] args) {
 
 	}
 
@@ -22,12 +20,49 @@ public class AddTwoNumbers{
 		ListNode(int x) { val = x; }
 	}
 
+	static public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		ListNode before = new ListNode(0);
+		ListNode node = before;
+		int carry = 0;
+
+		while (l1 != null && l2 != null) {
+			int val = l1.val + l2.val + carry;
+			node.next = new ListNode(val % 10);
+			node = node.next;
+			carry = val / 10;
+			l1 = l1.next;
+			l2 = l2.next;
+		}
+
+		while (l1 != null) {
+			int val = l1.val + carry;
+			node.next = new ListNode(val % 10);
+			node = node.next;
+			carry = val / 10;
+			l1 = l1.next;
+		}
+
+		while (l2 != null) {
+			int val = l2.val + carry;
+			node.next = new ListNode(val % 10);
+			node = node.next;
+			carry = val / 10;
+			l2 = l2.next;
+		}
+
+		if (carry > 0) {
+			node.next = new ListNode(carry);
+		}
+		
+		return before.next;
+	}
+
 	static public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		ListNode beforeHead = new ListNode(0);
 		ListNode tail = beforeHead;
 		int digit = 0;
 
-		while(l1 != null && l2 != null){
+		while (l1 != null && l2 != null) {
 			digit = l1.val + l2.val + digit;
 			tail.next = new ListNode(digit % 10);
 			tail = tail.next;
@@ -36,34 +71,34 @@ public class AddTwoNumbers{
 			digit /= 10;
 		}
 
-		while(digit == 1){
-			if(l1 != null){
+		while (digit == 1) {
+			if (l1 != null) {
 				digit = l1.val + digit;
 				tail.next = new ListNode(digit % 10);
 				tail = tail.next;
 				l1 = l1.next;
 				digit /= 10;
-			}else if(l2 != null){
+			} else if (l2 != null) {
 				digit = l2.val + digit;
 				tail.next = new ListNode(digit % 10);
 				tail = tail.next;
 				l2 = l2.next;
 				digit /= 10;
-			}else{
+			} else {
 				tail.next = new ListNode(digit);
 				digit = 0;
 			}
 		}
 
-		while(l1 != null){
+		while (l1 != null) {
 			tail.next = new ListNode(l1.val );
-			tail = tail.next; 
+			tail = tail.next;
 			l1 = l1.next;
 		}
 
-		while(l2 != null){
+		while (l2 != null) {
 			tail.next = new ListNode(l2.val );
-			tail = tail.next; 
+			tail = tail.next;
 			l2 = l2.next;
 		}
 		return beforeHead.next;
