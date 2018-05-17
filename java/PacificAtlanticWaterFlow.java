b3b8472b6f61fe763ac915f413f3a2f74c88a78c
jonathanz87 <jonathanz@foxmail.com>
2018-05-17 00:46:58 -0700

    problem 417

--- java/PacificAtlanticWaterFlow.java
+++ java/PacificAtlanticWaterFlow.java
@@ -26,8 +26,23 @@ import java.util.List;
 import java.util.LinkedList;
 
 public class PacificAtlanticWaterFlow {
-	public List<int[]> pacificAtlantic(int[][] matrix) {
-		if (matrix == null || matrix.size <= 0 || matrix[0].size <= 0) return new LinkedList<>();
+	public static void main(String[] args) {
+		int[][] matrix = new int[][] {
+			{1, 2, 2, 3, 5},
+			{3, 2, 3, 4, 4},
+			{2, 4, 5, 3, 1},
+			{6, 7, 1, 4, 5},
+			{5, 1, 1, 2, 4}
+		};
+
+		List<int[]> list = pacificAtlantic(matrix);
+		for (int[] i : list) {
+			System.out.println(i[0] + " - " + i[1]);
+		}
+	}
+
+	public static List<int[]> pacificAtlantic(int[][] matrix) {
+		if (matrix == null || matrix.length <= 0 || matrix[0].length <= 0) return new LinkedList<>();
 
 		int m = matrix.length, n = matrix[0].length;
 		int[] xQueue = new int[m * n];
@@ -36,41 +51,88 @@ public class PacificAtlanticWaterFlow {
 		int queueIndex = -1;
 		boolean[][] pacific = new boolean[m][n];
 		boolean[][] atlantic = new boolean[m][n];
+		List<int[]> result = new LinkedList<>();
 
 		for (int j = 0; j < n; j++) {
-			queueSize++;
-			xQueue[queueSize] = 0;
-			yQueue[queueSize] = j;
-			pacific[0][j] = true;
+			update(xQueue, yQueue, ++queueSize, pacific, 0, j);
 		}
 
-		for(int i = 1; i < m; i++){
-			queueSize++;
-			xQueue[queueSize] = i;
-			yQueue[queueSize] = 0;
-			pacific[i][0] = true;
+		for (int i = 1; i < m; i++) {
+			update(xQueue, yQueue, ++queueSize, pacific, i, 0);
 		}
 
-		while(queueIndex <= queueSize){
+		while (queueIndex < queueSize) {
 			queueIndex++;
 			int x = xQueue[queueIndex];
 			int y = yQueue[queueIndex];
-			if(flowable(matrix, matrix[x][y], x + 1, y)){
-				queueSize++;
-				xQueue[queueSize] = x + 1;
-				yQueue[queueSize] = y;
+			if (flowable(matrix, matrix[x][y], x + 1, y) && !pacific[x + 1][y]) {
+				update(xQueue, yQueue, ++queueSize, pacific, x + 1, y);
 			}
 
+			if (flowable(matrix, matrix[x][y], x - 1, y) && !pacific[x - 1][y]) {
+				update(xQueue, yQueue, ++queueSize, pacific, x - 1, y);
+			}
+
+			if (flowable(matrix, matrix[x][y], x, y + 1) && !pacific[x][y + 1]) {
+				update(xQueue, yQueue, ++queueSize, pacific, x, y + 1);
+			}
+
+			if (flowable(matrix, matrix[x][y], x, y - 1) && !pacific[x][y - 1]) {
+				update(xQueue, yQueue, ++queueSize, pacific, x, y - 1);
+			}
 
 		}
 
-		private boolean flowable(int[][] matrix, int height, int x, int y){
-			if(x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length){
-				return false;
-			}
-			return height <= matrix[x][y];
+		queueSize = -1;
+		queueIndex = -1;
+		for (int j = 0, i = m - 1; j < n; j++) {
+			update(xQueue, yQueue, ++queueSize, atlantic, i, j);
 		}
 
-		private void update(int q)
+		for (int i = m - 2, j = n - 1; i >= 0; i--) {
+			update(xQueue, yQueue, ++queueSize, atlantic, i, j);
+		}
+
+		while (queueIndex < queueSize) {
+			queueIndex++;
+			int x = xQueue[queueIndex];
+			int y = yQueue[queueIndex];
+			if (pacific[x][y]) {
+				result.add(new int[] {x, y});
+			}
+
+			if (flowable(matrix, matrix[x][y], x + 1, y) && !atlantic[x + 1][y]) {
+				update(xQueue, yQueue, ++queueSize, atlantic, x + 1, y);
+			}
+
+			if (flowable(matrix, matrix[x][y], x - 1, y) && !atlantic[x - 1][y]) {
+				update(xQueue, yQueue, ++queueSize, atlantic, x - 1, y);
+			}
+
+			if (flowable(matrix, matrix[x][y], x, y + 1) && !atlantic[x][y + 1]) {
+				update(xQueue, yQueue, ++queueSize, atlantic, x, y + 1);
+			}
+
+			if (flowable(matrix, matrix[x][y], x, y - 1) && !atlantic[x][y - 1]) {
+				update(xQueue, yQueue, ++queueSize, atlantic, x, y - 1);
+			}
+		}
+
+		return result;
+	}
+
+
+	private static boolean flowable(int[][] matrix, int height, int x, int y) {
+		if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length) {
+			return false;
+		}
+		return height <= matrix[x][y];
+	}
+
+	private static void update(int[] xQueue, int[] yQueue, int index, boolean[][] visited, int x, int y) {
+		xQueue[index] = x;
+		yQueue[index] = y;
+
+		visited[x][y] = true;
 	}
 }
