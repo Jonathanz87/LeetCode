dc2c367b3140cd58e44b731fdf72975f11bff7c2
jonathanz87 <jonathanz@foxmail.com>
2018-06-05 22:09:03 -0700

    problem 777 Swap Adjacent in LR String

--- java/SwapAdjacentInLrString.java
+++ java/SwapAdjacentInLrString.java
@@ -4,72 +4,17 @@ public class SwapAdjacentInLrString {
 	}
 	public static boolean canTransform(String start, String end) {
 		if (start.length() != end.length()) return false;
-		int ct = 0, size = start.length();
-		int sIndex = 0, eIndex = 0;
+		int l = 0, r = 0;
 
-		while (sIndex < size && eIndex < size) {
-			if (start.charAt(sIndex) == 'L') {
-				sIndex++;
-				if (ct > 0) {
-					ct--;
-					continue;
-				}
-				if (end.charAt(eIndex) == 'L') {
-					eIndex++;
-					continue;
-				}
-				System.out.println("1 " + sIndex + " - " + eIndex);
-				return false;
-			}
-			if (end.charAt(eIndex) == 'R') {
-				eIndex++;
-				if (ct < 0) {
-					ct++;
-					continue;
-				}
-				if (start.charAt(sIndex) == 'R') {
-					sIndex++;
-					continue;
-				}
-								System.out.println("2 " + sIndex + " - " + eIndex);
-				return false;
-			}
+		for (int i = 0, size = start.length(); i < size; i++) {
+			if (start.charAt(i) == 'R') r++;
+			else if (start.charAt(i) == 'L') l--;
+			if (end.charAt(i) == 'L') l++;
+			else if (end.charAt(i) == 'R') r--;
 
-			if (start.charAt(sIndex) == 'R' && end.charAt(eIndex) == 'L') {
-								System.out.println("3 " + sIndex + " - " + eIndex);
-				return false;
-			}
-
-			if (start.charAt(sIndex) == 'R') {
-				ct--;
-				sIndex++;
-				continue;
-			}
-			if (end.charAt(eIndex) == 'L') {
-				ct++;
-				eIndex++;
-				continue;
-			}
-			sIndex++;
-			eIndex++;
+			if (r < 0 || l < 0 || (r > 0 && l > 0)) return false;
 		}
 
-		while (sIndex < size) {
-			if (start.charAt(sIndex) != 'L' || ct <= 0) {
-								System.out.println("4 " + sIndex + " - " + eIndex);
-				return false;
-			}
-			sIndex++;
-			ct --;
-		}
-		while (eIndex < size) {
-			if (end.charAt(eIndex) != 'R' || ct >= 0) {
-								System.out.println("5 " + sIndex + " - " + eIndex);
-				return false;
-			}
-			eIndex++;
-			ct ++;
-		}
-		return true;
+		return l == 0 && r == 0;
 	}
 }
