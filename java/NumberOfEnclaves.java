7c0434ab871b821e5901c93b6870246092c45b41
jonathanz87 <jonathanz@foxmail.com>
2019-05-27 12:49:18 -0700

    1020. Number of Enclaves

--- java/NumberOfEnclaves.java
+++ java/NumberOfEnclaves.java
@@ -1,4 +1,5 @@
 /*
+    1020. Number of Enclaves
     Given a 2D array A, each cell is 0 (representing sea) or 1 (representing land)
     A move consists of walking from one land square 4-directionally to another land square, or off the boundary of the grid.
     Return the number of land squares in the grid for which we cannot walk off the boundary of the grid in any number of moves.
