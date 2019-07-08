6cadd31720705ef02cd675526e246884d6bf2f27
Jonathanz87 <Jonathanz87@users.noreply.github.com>
2019-07-07 18:23:35 -0700

    1046. Last Stone Weight

---
+++ java/LastStoneWeight.java
@@ -0,0 +1,46 @@
+
+/*
+    1046. Last Stone Weight
+    We have a collection of rocks, each rock has a positive integer weight.
+    Each turn, we choose the two heaviest rocks and smash them together. 
+    Suppose the stones have weights x and y with x <= y.
+    The result of this smash is:
+    If x == y, both stones are totally destroyed;
+    If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+    At the end, there is at most 1 stone left.
+    Return the weight of this stone (or 0 if there are no stones left.)
+    Example 1:
+        Input: [2,7,4,1,8,1]
+        Output: 1
+        Explanation: 
+        We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+        we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+        we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+        we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+    Note:
+        1 <= stones.length <= 30
+        1 <= stones[i] <= 1000
+*/
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(stones.length, Collections.reverseOrder());
+        for (int n : stones) {
+            pQueue.add(n);
+        }
+
+        while (pQueue.size() > 1) {
+            int i1 = pQueue.poll();
+            int i2 = pQueue.poll();
+            int result = i1 - i2;
+            if (result != 0) {
+                pQueue.add(result);
+            }
+        }
+
+        return pQueue.isEmpty() ? 0 : pQueue.poll();
+    }
+}
