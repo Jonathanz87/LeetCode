a5f7d727ebd4371e9f2a834073a9eb50fb6157cc
jonathanz87 <jonathanz@foxmail.com>
2019-08-31 11:39:04 -0700

    66. Plus One

---
+++ go/0066_PlusOne.go
@@ -0,0 +1,39 @@
+/*
+
+	Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+	The digits are stored such that the most significant digit is at the head of the list, 
+	and each element in the array contain a single digit.
+	You may assume the integer does not contain any leading zero, except the number 0 itself.
+	Example 1:
+		Input: [1,2,3]
+		Output: [1,2,4]
+		Explanation: The array represents the integer 123.
+	Example 2:
+		Input: [4,3,2,1]
+		Output: [4,3,2,2]
+		Explanation: The array represents the integer 4321.
+*/
+
+package main
+
+func plusOne(digits []int) []int {
+	carry := 1
+	
+	for i := len(digits) - 1; i >= 0; i-- {
+		carry = digits[i] + carry
+		digits[i] = carry % 10
+		carry = carry / 10
+	}
+
+	if carry == 1 {
+		copy := []int{1}
+
+		for _, d := range digits {
+			copy = append(copy, d)
+		}
+
+		digits = copy
+	}
+
+	return digits
+}
